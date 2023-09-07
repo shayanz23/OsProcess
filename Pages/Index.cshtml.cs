073b76ca76f5b431cd1634ec696b65ffbe86eec0
shayanz <shayan.zahedana@gmail.com>
2023-09-07 14:32:23 -0700

    total cpu BS

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -17,5 +17,27 @@ public class IndexModel : PageModel
     {
         Process[] processes = Process.GetProcesses();
         ViewData["P"] = processes;
+        double totalRam = 0;
+        double totalCPU = 0;
+        cupuusage(processes, ref totalRam, ref totalCPU);
+        ViewData["totalRam"] = totalRam;
+        ViewData["totalCPU"] = totalCPU;
+    }
+
+    void cupuusage(Process[] processes, ref double total, ref double totalCPU)
+    {
+        foreach (Process process in processes)
+        {
+            try
+            {
+                total += process.WorkingSet64/1000000;
+                totalCPU += process.TotalProcessorTime.TotalHours;
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        return;
     }
 }
